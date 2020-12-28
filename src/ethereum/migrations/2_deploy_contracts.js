9b14f1fa9a5bce3d8be219ad101bbd0de2ee06ea
ga58kam <metodi.manov@tum.de>
2020-12-28 19:06:06 +0200

    deploy the registry to the public test blockchain network

--- src/ethereum/migrations/2_deploy_contracts.js
+++ src/ethereum/migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 const TeSCRegistryImplementation = artifacts.require("TeSCRegistryImplementation");
-const ExampleTeSCContract = artifacts.require("ExampleTeSCContract");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(ExampleTeSCContract, "customdomain.com", "1924819200", "0x0", "customsignatureBEGINEND");
     await deployer.deploy(TeSCRegistryImplementation);
 };
