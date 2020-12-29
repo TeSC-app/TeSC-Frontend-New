624b326e715722002899620fa33638a383a4ffff
T.A. Ma <anhmt90@gmail.com>
2020-12-29 18:01:37 +0100

    refactor tesc new

---
+++ src/components/SearchBox.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect, useContext, useCallback } from 'react';
+import { Input, Table, Checkbox, Loader, Icon, Label, Grid, Card, Form, Dimmer } from 'semantic-ui-react';
+
+
+
+const SearchBox = ({ handleSubmit, placeholder }) => {
+    
+    
+}
