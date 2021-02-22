0b98c84add0bbb0a9164b19a3811ab53f0c5fc34
dangerdave1 <david.stuebing@gmx.de>
2021-02-22 12:18:48 +0100

    Minor image changes, rename to registry analytics on landing page

--- src/landingPage/components/sections/FeaturesSplit.js
+++ src/landingPage/components/sections/FeaturesSplit.js
@@ -235,7 +235,7 @@ const FeaturesSplit = ({
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Registry-wide Analytics
+                  Registry Analytics
                   </div>
                 <h3 className="mt-0 mb-12">
                   Statistics for TeSC Registry
